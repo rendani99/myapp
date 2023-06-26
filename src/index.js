13f4a09e78b1ca6dc1ab2a53b47d3bf0aad35ba9
rendani99 <rendania460@gmail.com>
2023-06-26 11:51:49 +0200

    start again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+/* The following line can be included in src/index.js or App.js file */
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
